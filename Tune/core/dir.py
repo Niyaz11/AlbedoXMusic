c2091c93ba390dd17e1b52fe3862229ef0d5a621
Rajnish <vedanyadevine@gmail.com>
2025-10-15 21:47:35 +0530

    Initial commit

---
+++ Tune/core/dir.py
@@ -0,0 +1,16 @@
+import os
+from Tune.logging import LOGGER
+
+BASE_DIR = os.getcwd()
+DOWNLOAD_DIR = os.path.join(BASE_DIR, "downloads")
+CACHE_DIR = os.path.join(BASE_DIR, "cache")
+
+def dirr():
+    for file in os.listdir():
+        if file.lower().endswith((".jpg", ".jpeg", ".png")):
+            os.remove(file)
+
+    os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+    os.makedirs(CACHE_DIR, exist_ok=True)
+
+    LOGGER(__name__).info("Directories Updated.")
